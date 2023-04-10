8a9d937416ac1cbbe67acc45353306b2a15f1b8b
dcherian <deepak@cherian.net>
2023-04-10 09:52:29 -0600

    Org-latex: Better link target names

--- config.el
+++ config.el
@@ -240,6 +240,7 @@
 (use-package! spell-fu
   :config
   (setq ispell-dictionary "english"
+        spell-fu-idle-delay 0.5 ; default is 0.25
         ispell-personal-dictionary "~/.doom.d/dictionary.pws")
   )
 
@@ -290,7 +291,93 @@
   ;; add to export hook
   (add-hook 'org-export-before-processing-hook 'delete-org-comments)
 
-   )
+  ;; https://github.com/alphapapa/unpackaged.el#export-to-html-with-useful-anchors
+  (define-minor-mode unpackaged/org-export-html-with-useful-ids-mode
+    "Attempt to export Org as HTML with useful link IDs.
+Instead of random IDs like \"#orga1b2c3\", use heading titles,
+made unique when necessary."
+    :global t
+    (if unpackaged/org-export-html-with-useful-ids-mode
+        (advice-add #'org-export-get-reference :override #'unpackaged/org-export-get-reference)
+      (advice-remove #'org-export-get-reference #'unpackaged/org-export-get-reference)))
+
+  (defun unpackaged/org-export-get-reference (datum info)
+    "Like `org-export-get-reference', except uses heading titles instead of random numbers."
+    (let ((cache (plist-get info :internal-references)))
+      (or (car (rassq datum cache))
+          (let* ((crossrefs (plist-get info :crossrefs))
+                 (cells (org-export-search-cells datum))
+                 ;; Preserve any pre-existing association between
+                 ;; a search cell and a reference, i.e., when some
+                 ;; previously published document referenced a location
+                 ;; within current file (see
+                 ;; `org-publish-resolve-external-link').
+                 ;;
+                 ;; However, there is no guarantee that search cells are
+                 ;; unique, e.g., there might be duplicate custom ID or
+                 ;; two headings with the same title in the file.
+                 ;;
+                 ;; As a consequence, before re-using any reference to
+                 ;; an element or object, we check that it doesn't refer
+                 ;; to a previous element or object.
+                 (new (or (cl-some
+                           (lambda (cell)
+                             (let ((stored (cdr (assoc cell crossrefs))))
+                               (when stored
+                                 (let ((old (org-export-format-reference stored)))
+                                   (and (not (assoc old cache)) stored)))))
+                           cells)
+                          (when (org-element-property :raw-value datum)
+                            ;; Heading with a title
+                            (unpackaged/org-export-new-title-reference datum cache))
+                          ;; NOTE: This probably breaks some Org Export
+                          ;; feature, but if it does what I need, fine.
+                          (org-export-format-reference
+                           (org-export-new-reference cache))))
+                 (reference-string new))
+            ;; Cache contains both data already associated to
+            ;; a reference and in-use internal references, so as to make
+            ;; unique references.
+            (dolist (cell cells) (push (cons cell new) cache))
+            ;; Retain a direct association between reference string and
+            ;; DATUM since (1) not every object or element can be given
+            ;; a search cell (2) it permits quick lookup.
+            (push (cons reference-string datum) cache)
+            (plist-put info :internal-references cache)
+            reference-string))))
+
+  (defun unpackaged/org-export-new-title-reference (datum cache)
+    "Return new reference for DATUM that is unique in CACHE."
+    (cl-macrolet ((inc-suffixf (place)
+                    `(progn
+                       (string-match (rx bos
+                                         (minimal-match (group (1+ anything)))
+                                         (optional "--" (group (1+ digit)))
+                                         eos)
+                                     ,place)
+                       ;; HACK: `s1' instead of a gensym.
+                       (-let* (((s1 suffix) (list (match-string 1 ,place)
+                                                  (match-string 2 ,place)))
+                               (suffix (if suffix
+                                           (string-to-number suffix)
+                                         0)))
+                         (setf ,place (format "%s--%s" s1 (cl-incf suffix)))))))
+      (let* ((title (org-element-property :raw-value datum))
+             (ref (url-hexify-string (substring-no-properties title)))
+             (parent (org-element-property :parent datum)))
+        (while (--any (equal ref (car it))
+                      cache)
+          ;; Title not unique: make it so.
+          (if parent
+              ;; Append ancestor title.
+              (setf title (concat (org-element-property :raw-value parent)
+                                  "--" title)
+                    ref (url-hexify-string (substring-no-properties title))
+                    parent (org-element-property :parent parent))
+            ;; No more ancestors: add and increment a number.
+            (inc-suffixf ref)))
+        ref)))
+)
 
 (use-package! ox-extra
   :after org
@@ -434,3 +521,37 @@
 
 (after! org
   (remove-hook 'after-save-hook #'+literate|recompile-maybe))
+
+(use-package! magit-delta
+  :if (executable-find "delta")
+  :hook (magit-mode . magit-delta-mode)
+  :config
+  ; Kanged https://github.com/dandavison/magit-delta/issues/13
+  (setq magit-delta-delta-args (append magit-delta-delta-args '("--features" "magit-delta")))
+  ; Kanged https://github.com/dandavison/magit-delta/issues/15#issuecomment-865774240
+  (defun hz/magit-delta-toggle ()
+    "Toggle magit-delta-mode and refresh magit."
+    (interactive)
+    (progn
+      (call-interactively 'magit-delta-mode)
+      (magit-refresh)))
+  :general
+  (:keymaps 'magit-mode-map
+            :states 'normal
+            :prefix ","
+            "t" '(hz/magit-delta-toggle :wk "toggle magit-delta"))
+
+  (defvar hz/magit-delta-point-max 50000)
+  ;; Disable mode if there are too many characters
+  (advice-add 'magit-delta-call-delta-and-convert-ansi-escape-sequences :around
+              (defun hz/magit-delta-colorize-maybe-a (fn &rest args)
+                (if (<= (point-max) hz/magit-delta-point-max)
+                    (apply fn args)
+                  (magit-delta-mode -1))))
+  ;; Re-enable mode after `magit-refresh' if there aren't too many characters
+  (add-hook 'magit-post-refresh-hook
+            (defun hz/magit-enable-magit-delta-maybe-h (&rest _args)
+              (when (and (not magit-delta-mode)
+                         (<= (point-max) hz/magit-delta-point-max))
+                (magit-delta-mode +1))))
+)
